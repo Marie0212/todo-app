1abf77c1420db9679bb9a00eb9b27f66651c2313
majdalkahwaji8-jpg <majd@example.com>
2026-01-19 16:46:43 +0100

    US-07: Fix build (TaskDeleter + remove IsNullOrWhiteSpace)

--- src/main/java/de/todoapp/Main.java
+++ src/main/java/de/todoapp/Main.java
@@ -1,20 +1,18 @@
-package de.todoapp;
+﻿package de.todoapp;
 
-import de.todoapp.persistence.InMemoryCategoryRepository;
-import de.todoapp.persistence.InMemoryTaskRepository;
+import de.todoapp.persistence.SqliteDatabase;
+import de.todoapp.persistence.SqliteTaskRepository;
 import de.todoapp.presentation.ConsoleApp;
-import de.todoapp.service.CategoryService;
 import de.todoapp.service.TaskService;
 
 public class Main {
     public static void main(String[] args) {
-        var taskRepo = new InMemoryTaskRepository();
-        var taskService = new TaskService(taskRepo, taskRepo, taskRepo, taskRepo);
+        var db = new SqliteDatabase("jdbc:sqlite:data/todo.db");
+        db.initSchema();
 
-        var categoryRepo = new InMemoryCategoryRepository();
-        var categoryService = new CategoryService(categoryRepo, categoryRepo);
+        var repo = new SqliteTaskRepository(db);
+        var service = new TaskService(repo, repo, repo, repo);
 
-        var app = new ConsoleApp(taskService, taskService, categoryService, categoryService);
-        app.run();
+        new ConsoleApp(service, service).run();
     }
 }

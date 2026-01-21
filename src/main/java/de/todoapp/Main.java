98bad5ae299f5fe338a7170a216b9a8b9a618de7
علاء الحسن <alhsn@host10-21-196-63.wlan2.uni-saarland.local>
2026-01-21 18:02:02 +0100

    Fix app startup wiring for ConsoleApp and CategoryService

--- src/main/java/de/todoapp/Main.java
+++ src/main/java/de/todoapp/Main.java
@@ -1,15 +1,31 @@
 package de.todoapp;
 
+import de.todoapp.persistence.InMemoryCategoryRepository;
+import de.todoapp.persistence.InMemoryTaskRepository;
+import de.todoapp.presentation.ConsoleApp;
+import de.todoapp.service.CategoryService;
+import de.todoapp.service.TaskService;
+
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            Class<?> appClass = Class.forName("de.todoapp.presentation.ConsoleApp");
-            Object app = appClass.getDeclaredConstructor().newInstance();
-            appClass.getMethod("run").invoke(app);
-        } catch (Exception e) {
-            System.err.println("Fehler beim Starten der Anwendung:");
-            e.printStackTrace();
-        }
+
+        // Repos
+        InMemoryTaskRepository taskRepo = new InMemoryTaskRepository();
+        InMemoryCategoryRepository categoryRepo = new InMemoryCategoryRepository();
+
+        // Services
+        TaskService taskService = new TaskService(taskRepo, taskRepo, taskRepo, taskRepo);
+        CategoryService categoryService = new CategoryService(categoryRepo, categoryRepo);
+
+        // App (genau 4 Parameter, wie der Compiler es verlangt)
+        ConsoleApp app = new ConsoleApp(
+                taskService,      // TaskCommandService
+                taskService,      // TaskQueryService
+                categoryService,  // CategoryCommandService
+                categoryService   // CategoryQueryService
+        );
+
+        app.run();
     }
 }
